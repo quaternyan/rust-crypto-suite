272d587496708df06463846d8e4022cafd2ba26b
Crypto-Neko <objective.axioma@gmail.com>
2024-12-25 18:20:40 -0500

    RSA is now working!! RSA implementation is considered complete for now.

--- rsa/src/main.rs
+++ rsa/src/main.rs
@@ -1,7 +1,6 @@
 use std::io::{self, Write};
-use num_bigint::{BigUint};
+use num_bigint::{RandBigInt, BigUint, BigInt, ToBigUint, Sign};
 use num_traits::{Zero, One};
-use num_primes::Generator;
 
 // Compute m^pow mod n
 fn power_mod(m: BigUint, n: BigUint, pow: BigUint) -> BigUint {
@@ -21,39 +20,68 @@ fn power_mod(m: BigUint, n: BigUint, pow: BigUint) -> BigUint {
 
 // Gets the multiplicative inverse of e mod n
 fn mod_inverse(e: &BigUint, n: &BigUint) -> Option<BigUint> {
-    let mut t = BigUint::zero();
-    let mut tt = BigUint::one();
+    let mut t: BigInt = Zero::zero();
+    let mut tt: BigInt = One::one();
     let mut r = n.clone();
     let mut rr = e.clone();
 
-    while rr != BigUint::zero() {
-        let q = &r / &rr;
-        t = &t - &q * &tt;
-        r = &r - &q * &rr;
-
-        std::mem::swap(&mut t, &mut tt);
-        std::mem::swap(&mut r, &mut rr);
+    while rr != Zero::zero() {
+        let q = &r - (&r / &rr) * (&rr);
+        let v = &t - BigInt::from_biguint(Sign::Plus, &r/&rr) * &tt;
+        r = rr;
+        rr = q;
+        t = tt;
+        tt = v;
     }
 
     // Return the inverse or None if no inverse exists
-    if r > BigUint::one() {
+    if r > One::one() {
         None
     } else {
-        if t < BigUint::zero() {
-            t = t + n;
+        if t < Zero::zero() {
+            t = t + BigInt::from_biguint(Sign::Plus, n.clone());
         }
-        Some(t)
+        Some(t.to_biguint().expect("Error converting to BigUint"))
     }
+}
+
+// Returns true if a BigUint is prime, false if not
+fn is_prime(n: &BigUint) -> bool{
+    if n & 1_u8.to_biguint().unwrap() == 0_u8.to_biguint().unwrap() {
+        return false;
+    }
+
+    // Runs Fermat's test 10 times to determine if an int is prime
+    // The chance that the resulting int is not prime is less than 0.0001%
+    for _i in 0..10 {
+        let mut rng = rand::thread_rng();
+        let a = rng.gen_biguint_range(&0_u8.to_biguint().unwrap(), &(n-1u32));
+        let result = a.modpow(&(n-1u32), n);
+        if result != 1_u8.to_biguint().unwrap() {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Get a random prime of size bits
+fn get_rand_prime(bits: u64, lbound: u64) -> BigUint {
+    let mut rng = rand::thread_rng();
+    let mut a = rng.gen_biguint(bits);
+
+    while !is_prime(&(&a+lbound)) {
+        a = rng.gen_biguint(bits);
+    }
+    a+lbound
 }
 
 // Generate a key randomly
 fn key_gen(e: BigUint) -> (BigUint, BigUint, BigUint) {
-    let bits = 1024;
-    let temp_p: num_primes::BigUint = Generator::new_prime(bits);
-    let temp_q: num_primes::BigUint = Generator::new_prime(bits);
-    let p = BigUint::from_bytes_be(&temp_p.to_bytes_be());
-    let q = BigUint::from_bytes_be(&temp_q.to_bytes_be());
-    let n = &p *& q;
+    let bits: u64 = 1024;
+    let lbound: u64 = 2u64.pow(32);
+    let p = get_rand_prime(bits, lbound);
+    let q = get_rand_prime(bits, lbound);
+    let n = &p * &q;
     let phi_n = (&p - BigUint::one()) * (&q - BigUint::one());
     let d = mod_inverse(&e, &phi_n).expect("Failed to calcuate mod inverse.");
     (n, d, phi_n)
@@ -100,7 +128,7 @@ fn main() {
     let c = enc(m.clone(), e.clone(), n.clone());
 
     // Verify that the ciphertext decrypts to the message again
-    let ptxt = dec(c.clone(), d, n, phi_n);
+    let ptxt = dec(c.clone(), d.clone(), n.clone(), phi_n.clone());
 
     assert!(ptxt == m);
     println!("Encryption successful!");
